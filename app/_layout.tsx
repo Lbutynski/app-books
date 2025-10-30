71f1988b9304b6224f2ab2ac35cfd8b1b0a70c10
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2025-10-30 08:46:53 +0100

    update header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,12 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Books List" }} />
-      <Stack.Screen name="[bookId]" options={{ title: "Book Details" }} />
+      <Stack.Screen name="books/[bookId]" options={{ title: "Book Details" }} />
+      <Stack.Screen
+        name="books/[bookId]/edit"
+        options={{ title: "Edit Book" }}
+      />
+      <Stack.Screen name="addBook" options={{ title: "Add Book" }} />
     </Stack>
   );
 }
